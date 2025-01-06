e686268121eaf14d80e5e2fe7d00fc1c5eaa2fa7
Guillaume de Romemont <46521584+guigzair@users.noreply.github.com>
2025-01-06 17:29:59 +0100

    Energy and ensotrophy

--- NS_spectral.py
+++ NS_spectral.py
@@ -1,164 +1,212 @@
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+from torch_geometric.data import Data
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.animation as animation
 
-"""
-Create Your Own Navier-Stokes Spectral Method Simulation (With Python)
-Philip Mocz (2023), @PMocz
-
-Simulate the Navier-Stokes equations (incompressible viscous fluid) 
-with a Spectral method
-
-v_t + (v.nabla) v = nu * nabla^2 v + nabla P
-div(v) = 0
-
-"""
-
-
-def poisson_solve( rho, kSq_inv ):
-	""" solve the Poisson equation, given source field rho """
-	V_hat = -(np.fft.fftn( rho )) * kSq_inv
-	V = np.real(np.fft.ifftn(V_hat))
-	return V
+size = 14
+params = {
+    'text.usetex': True,
+    'font.family': 'serif',
+    'font.serif': 'cm',  # Computer Modern font
+	'legend.fontsize':size,
+    'axes.labelsize' : size,
+	'axes.titlesize' : size +2,
+    'xtick.labelsize' : size+1,
+    'ytick.labelsize' : size+1
+}
+plt.rcParams.update(params)
 
 
-def diffusion_solve( v, dt, nu, kSq ):
-	""" solve the diffusion equation over a timestep dt, given viscosity nu """
-	v_hat = (np.fft.fftn( v )) / (1.0+dt*nu*kSq)
-	v = np.real(np.fft.ifftn(v_hat))
-	return v
+################################################################
+######################### Device ###############################
+################################################################
+
+print(f"Is GPU available ? : {torch.cuda.is_available()}")
+print(f"Number of GPU devices : {torch.cuda.device_count()}")
+print(f"Current GPU devive : {torch.cuda.current_device()}")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"device : {device}")
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.set_default_dtype(torch.float32)
+
+################################################################
+####################### Parameters #############################
+################################################################
+
+class Params(Data):
+    def __cat_dim__(self, key, value, *args, **kwargs):
+        if key != 'mesh':
+            return None
+        return super().__cat_dim__(key, value, *args, **kwargs)
+
+# Parameters
+params = {
+    't_end': 0.1,  # End time
+    'Lx': 1.0, # Domain size
+    'Ly': 1.0,  
+    'N_x': 160, # Number of grid points
+    'N_y': 160,
+    'CFL' : 0.1, # CFL number
+    'n_modes': 32,  # Number of modes
+    'nu' : 0.1, # small-scale (hyper)-viscosity coefficient
+    'n_nu' : 1,  # order of the small-scale (hyper)-viscosity
+    'mu' : 0.01,  # viscosity coefficient
+    'n_mu' : 0,  # order of the viscosity
+    "F" : "kolmogorov" # Forcing type
+}
+
+params = torch.nn.ParameterDict(params)
 
 
-def grad(v, kx, ky):
-	""" return gradient of v """
-	v_hat = np.fft.fftn(v)
-	dvx = np.real(np.fft.ifftn( 1j*kx * v_hat))
-	dvy = np.real(np.fft.ifftn( 1j*ky * v_hat))
-	return dvx, dvy
+################################################################
+######################### Solve ################################
+################################################################
+
+# Simulation parameters
+N         = 400     # Spatial resolution
+t         = 0       # current time of the simulation
+tEnd      = 1       # time at which simulation ends
+dt        = 0.01   # timestep
+tOut      = 0.01    # draw frequency
+nu        = 2e-8   # viscosity
+n_nu      = 2
+mu        = 0   # viscosity
+n_mu      = 0
+
+# Domain [0,1] x [0,1]
+L = 2 * np.pi   
+xlin = np.linspace(0,L, num=N+1)  # Note: x=0 & x=1 are the same point!
+xlin = xlin[0:N]                  # chop off periodic point
+xx, yy = np.meshgrid(xlin, xlin)
+
+# Intial Condition (vortex)
+psi = -np.sin(2*np.pi*(yy+xx))
+psi_hat = np.fft.fftn( psi )
+vy =  np.sin(2*np.pi*xx*2) 
+
+# Fourier Space Variables
+klin = 2.0 * np.pi / L * np.arange(-N/2, N/2)
+kmax = np.max(klin)
+kx, ky = np.meshgrid(klin, klin)
+kx = np.fft.ifftshift(kx)
+ky = np.fft.ifftshift(ky)
+kSq = kx**2 + ky**2
+kSq_inv = 1.0 / kSq
+kSq_inv[kSq==0] = 1
+
+# dealias with the 2/3 rule
+dealias = (np.abs(kx) < (2./3.)*kmax) & (np.abs(ky) < (2./3.)*kmax)
 
 
-def div(vx, vy, kx, ky):
-	""" return divergence of (vx,vy) """
-	dvx_x = np.real(np.fft.ifftn( 1j*kx * np.fft.fftn(vx)))
-	dvy_y = np.real(np.fft.ifftn( 1j*ky * np.fft.fftn(vy)))
-	return dvx_x + dvy_y
+def getEnstrophy(psi_hat):
+    Z = 0.5 * np.sum(np.abs(kSq * psi_hat)**2)
+    return Z    
+    
+def getEnergy(psi_hat):
+    E = 0.5 * np.sum(kSq * np.abs(psi_hat)**2)
+    return E
 
 
-def curl(vx, vy, kx, ky):
-	""" return curl of (vx,vy) """
-	dvx_y = np.real(np.fft.ifftn( 1j*ky * np.fft.fftn(vx)))
-	dvy_x = np.real(np.fft.ifftn( 1j*kx * np.fft.fftn(vy)))
-	return dvy_x - dvx_y
+def initField(Amp = 0.5):
+    psi_hat = Amp * ((2 * np.random.rand(*xx.shape) - 1) + (2 * np.random.rand(*xx.shape) - 1)* 1j)
+    psi_hat = np.fft.fftn(np.real(np.fft.ifftn(psi_hat)))
+    return psi_hat
 
 
-def apply_dealias(f, dealias):
-	""" apply 2/3 rule dealias to field f """
-	f_hat = dealias * np.fft.fftn(f)
-	return np.real(np.fft.ifftn( f_hat ))
+def forcing(eps = 0.1 ):
+    forcing_wavenumber = 25.0 * 2 * np.pi/L  # the forcing wavenumber, `k_f`, for a spectrum that is a ring in wavenumber space
+    forcing_bandwidth  = 1.5  * 2 * np.pi/L  # the width of the forcing spectrum, `δ_f`
+
+    K = np.sqrt(kSq)
+    forcing_spectrum = np.exp(-(K - forcing_wavenumber)**2 / (2 * forcing_bandwidth**2))
+    forcing_spectrum[kSq == 0] = 0
+    eps_0 = np.sum(np.abs(forcing_spectrum)**2) / (L**2)
+
+    forcing_spectrum = forcing_spectrum * eps/eps_0
+
+    rand_val = np.random.rand(*kx.shape)
+    F_hat = np.sqrt(forcing_spectrum) * np.exp(1j * 2 * np.pi * rand_val) / np.sqrt(dt)
+    return F_hat
 
 
-def main():
-	""" Navier-Stokes Simulation """
-	
-	# Simulation parameters
-	N         = 400     # Spatial resolution
-	t         = 0       # current time of the simulation
-	tEnd      = 1       # time at which simulation ends
-	dt        = 0.001   # timestep
-	tOut      = 0.01    # draw frequency
-	nu        = 0.001   # viscosity
-	plotRealTime = True # switch on for plotting as the simulation goes along
-	
-	# Domain [0,1] x [0,1]
-	L = 1    
-	xlin = np.linspace(0,L, num=N+1)  # Note: x=0 & x=1 are the same point!
-	xlin = xlin[0:N]                  # chop off periodic point
-	xx, yy = np.meshgrid(xlin, xlin)
-	
-	# Intial Condition (vortex)
-	vx = -np.sin(2*np.pi*yy)
-	vy =  np.sin(2*np.pi*xx*2) 
-	
-	# Fourier Space Variables
-	klin = 2.0 * np.pi / L * np.arange(-N/2, N/2)
-	kmax = np.max(klin)
-	kx, ky = np.meshgrid(klin, klin)
-	kx = np.fft.ifftshift(kx)
-	ky = np.fft.ifftshift(ky)
-	kSq = kx**2 + ky**2
-	kSq_inv = 1.0 / kSq
-	kSq_inv[kSq==0] = 1
-	
-	# dealias with the 2/3 rule
-	dealias = (np.abs(kx) < (2./3.)*kmax) & (np.abs(ky) < (2./3.)*kmax)
-	
-	# number of timesteps
-	Nt = int(np.ceil(tEnd/dt))
-	
-	# prep figure
-	fig = plt.figure(figsize=(4,4), dpi=80)
-	outputCount = 1
-	
-	#Main Loop
-	for i in range(Nt):
 
-		# Advection: rhs = -(v.grad)v
-		dvx_x, dvx_y = grad(vx, kx, ky)
-		dvy_x, dvy_y = grad(vy, kx, ky)
-		
-		rhs_x = -(vx * dvx_x + vy * dvx_y)
-		rhs_y = -(vx * dvy_x + vy * dvy_y)
-		
-		rhs_x = apply_dealias(rhs_x, dealias)
-		rhs_y = apply_dealias(rhs_y, dealias)
+def Linear():
+    L_hat = - nu * kSq ** n_nu 
+    L_hat = L_hat - mu 
+    return L_hat
 
-		vx += dt * rhs_x
-		vy += dt * rhs_y
-		
-		# Poisson solve for pressure
-		div_rhs = div(rhs_x, rhs_y, kx, ky)
-		P = poisson_solve( div_rhs, kSq_inv )
-		dPx, dPy = grad(P, kx, ky)
-		
-		# Correction (to eliminate divergence component of velocity)
-		vx += - dt * dPx
-		vy += - dt * dPy
-		
-		# Diffusion solve (implicit)
-		vx = diffusion_solve( vx, dt, nu, kSq )
-		vy = diffusion_solve( vy, dt, nu, kSq )
-		
-		# vorticity (for plotting)
-		wz = curl(vx, vy, kx, ky)
-		
-		# update time
-		t += dt
-		print(t)
-		
-		# plot in real time
-		plotThisTurn = False
-		if t + dt > outputCount*tOut:
-			plotThisTurn = True
-		if (plotRealTime and plotThisTurn) or (i == Nt-1):
-			
-			plt.cla()
-			plt.imshow(wz, cmap = 'RdBu')
-			plt.clim(-20,20)
-			ax = plt.gca()
-			ax.invert_yaxis()
-			ax.get_xaxis().set_visible(False)
-			ax.get_yaxis().set_visible(False)	
-			ax.set_aspect('equal')	
-			plt.pause(0.001)
-			outputCount += 1
-			
-			
-	# Save figure
-	plt.savefig('navier-stokes-spectral.png',dpi=240)
-	plt.show()
-	
-	return 0
-	
+def NonLinear(psi_hat):
+    # forcing
+    F_hat = forcing(eps = 0.)
+
+    u = np.real(np.fft.ifftn(1j * ky * psi_hat))
+    v = np.real(np.fft.ifftn(- 1j * kx * psi_hat))
+    zeta = np.real(np.fft.ifftn( kSq * psi_hat))
+
+    NL_hat = 1j * kx * np.fft.fftn( u * zeta ) + 1j * ky * np.fft.fftn( v * zeta )
+    NL_hat = kSq_inv * (NL_hat - F_hat)
+    return NL_hat
+
+def time_step(psi_hat, dt):
+    L_hat = Linear()
+    NL_hat = NonLinear(psi_hat)
+
+    # imex
+    psi_hat = (psi_hat - dt * NL_hat)/(1 - dt * L_hat)
+
+    # dealiasing
+    psi_hat = dealias * psi_hat
+
+    return psi_hat
+
+np.random.seed(1234)
+psi_hat = initField(Amp = 0.5)
+# psi_hat = psi_hat * 0
+frames = []
+E = []
+Z = []
+for t in range(500):
+    psi_hat = time_step(psi_hat, dt)
+    E.append(getEnergy(psi_hat))
+    Z.append(getEnstrophy(psi_hat))
+    # if t % 10 == 0:
+    #     frames.append(np.real(np.fft.ifftn(psi_hat * kSq)))
+
+psi = np.real(np.fft.ifftn(psi_hat * kSq ))
 
 
-if __name__== "__main__":
-  main()
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+cs = ax.contourf(xx, yy, psi, levels = 20)
+cbar = fig.colorbar(cs)
+
+E = np.array(E)
+fig, ax = plt.subplots()
+ax.plot(E/E[0])
+ax.set_xlabel('Time')
+ax.set_ylabel('Energy')
+ax.grid()
+
+Z = np.array(Z)
+fig, ax = plt.subplots()
+ax.plot(Z/Z[0])
+ax.set_xlabel('Time')
+ax.set_ylabel('Energy')
+ax.grid()
+
+# Animation
+# fig, ax = plt.subplots()
+
+# ims = []
+# for i in range(len(frames)):
+#     im = ax.imshow(frames[i], animated = True)
+#     if i == 0:
+#         ax.imshow(frames[i])  # show an initial one first
+#     ims.append([im])
+
+# ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+#                                 repeat_delay=1000)
+# ani.save("animation.gif",writer = animation.PillowWriter(fps=10) )
